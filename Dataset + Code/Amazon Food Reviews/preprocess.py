5f78fd45d72c8a75f18dfa6cad1a426d3af7c4bd
fajim1 <fahim.ishrak1@gmail.com>
2020-11-09 15:46:55 -0500

    Update

---
+++ Dataset + Code/Amazon Food Reviews/preprocess.py
@@ -0,0 +1,59 @@
+from __future__ import print_function
+import sklearn
+
+#%%
+import lime
+import os
+import numpy as np
+import pandas as pd
+pd.set_option('display.max_colwidth', -1)
+pd.set_option('display.max_columns', None)
+import sklearn
+import sklearn.ensemble
+import sklearn.metrics
+import seaborn as sns
+from scipy.special import softmax
+import matplotlib.pyplot as plt
+from sklearn.utils import resample
+
+from transformers import BertTokenizer, BertForSequenceClassification, Trainer, TrainingArguments
+from transformers import RobertaTokenizer, RobertaForSequenceClassification
+from transformers import AlbertTokenizer, AlbertForSequenceClassification
+
+import torch
+import torch.nn as nn
+
+import torch
+
+
+# %%
+
+df_AR = pd.read_csv('Dataset/Amazon Food Reviews/Reviews.csv',nrows = 2000)
+
+df_AR = df_AR.iloc[:,[6,8]]
+
+df_AR['Score'] = df_AR['Score']-1
+df_AR['Score'].unique()
+
+#%%
+
+zero = df_AR[df_AR.Score==0]
+one = df_AR[df_AR.Score==1]
+two = df_AR[df_AR.Score==2]
+three = df_AR[df_AR.Score==3]
+four = df_AR[df_AR.Score==4]
+
+# upsample minority
+four_u = resample(four,
+                  replace=True, # sample with replacement
+                  n_samples=len(three), # match number with majority class
+                  random_state=42) # reproducible results
+
+
+# combine majority and upsampled minority
+df_AR = pd.concat([zero,one,two,three,four_u])
+#%%
+sns.countplot(df_AR['Score'])
+plt.show()
+#%%
+df_AR.to_csv('Dataset/Amazon Food Reviews/processed_data/Preprocess.csv', index=False)
